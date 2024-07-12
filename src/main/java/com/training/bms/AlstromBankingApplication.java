f83b0615efd9425bbef3ed2c624b5260b6f4d3fb
Pooja Shri <you@example.com>
2024-07-12 12:17:41 +0530

    third commit

--- src/main/java/com/training/bms/AlstromBankingApplication.java
+++ src/main/java/com/training/bms/AlstromBankingApplication.java
@@ -9,6 +9,7 @@ public class AlstromBankingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AlstromBankingApplication.class, args);
 		System.out.println("Alstrom banking app is initialized");
+		System.out.println("Alstrom banking app is executed");
 	}
 
 }
